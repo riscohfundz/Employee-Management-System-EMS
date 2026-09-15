89a30b50674fb98ee9f3095492977fbc7e0b1473
Riscoh Tech <riscohfundzgmail.com>
2026-09-15 16:57:21 +0100

    Configure backend for local and Vercel

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import multer from "multer";
 import connectDB from "./config/db.js";
+
 import authRouter from "./routes/authRoute.js";
 import employeeRouter from "./routes/employeeRouter.js";
 import profileRouter from "./routes/profileRoutes.js";
@@ -12,33 +13,28 @@ import payslipRoutes from "./routes/payslipRoutes.js";
 import dashboardRouter from "./routes/dashboardRoutes.js";
 
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
 
-
-
-const app = express()
-const PORT = process.env.PORT || 4000;
-
+const app = express();
 
 // Middleware
-
-app.use(cors())
-app.use(express.json())
-app.use(multer().none())
+app.use(cors());
+app.use(express.json());
+app.use(multer().none());
 
 // Routes
+app.get("/", (req, res) => res.send("Server is running"));
 
-app.get("/", (req, res) => res.send("Server is running"))
-app.use("/api/auth", authRouter)
-app.use("/api/employee", employeeRouter)
-app.use("/api/profile", profileRouter)
-app.use("/api/attendance", attendanceRouter)
-app.use("/api/leave", leaveRouter)
-app.use("/api/payslips", payslipRoutes)
-app.use("/api/dashboard", dashboardRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/employee", employeeRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/attendance", attendanceRouter);
+app.use("/api/leave", leaveRouter);
+app.use("/api/payslips", payslipRoutes);
+app.use("/api/dashboard", dashboardRouter);
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-await connectDB()
-app.listen(PORT, () => console.log(`Server running on port ${PORT} `)
-)
+await connectDB();
+
+export default app;
