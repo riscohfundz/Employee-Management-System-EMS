63689128bf7d576b703449203bc2e87c98bf5cb9
Riscoh Tech <riscohfundzgmail.com>
2026-09-15 12:01:31 +0100

    script added for creating admin

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { protect } from "../middleware/auth";
+import { protect } from "../middleware/auth.js";
 import { getDashborad } from "../controller/dashboardController.js";
 
 const dashboardRouter = Router();
